58948ede4b98b3d3b74c0812a336b0b8efe9d79d
Will R <will.ruizdecastilla@gmail.com>
2020-10-06 07:50:24 -0500

    added Action filter to update last active field whenever a User calls Controller to retreive or update User data

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.DTOs;
+using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
-
-    [Authorize]                         //Any access to this controller requires authorization
+    [ServiceFilter(typeof(LogUserActivity))]    //Any access to this controller 
+    [Authorize]                                 //requires ActionFilter + authorization    
     [Route("api/[Controller]")]
     [ApiController]
     public class UsersController : ControllerBase
